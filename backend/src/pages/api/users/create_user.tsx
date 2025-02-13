bbc1fbc97c1389d241706c877c54e688b79dd586
4COLE <87451237+4COLE@users.noreply.github.com>
2025-02-13 12:38:07 -0500

    last redo, didn't know that frontend and backend can be within the same nextjs app

--- backend/src/pages/api/users/create_user.tsx
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-import { connectToDatabase } from "../../../lib/mongodb";
-
-export async function createUser(email: string, password: string) {
-  const { db } = await connectToDatabase();
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-  await db.collection("users").insertOne({ email, passwordHash });
-}
